24945e3a210c8db70caade877dcd0a3e6f4a8a43
Guilherme Fontenele <guilherme@fontenele.net>
2025-07-06 20:06:55 -0300

    feature: env variable test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,114 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import * as process from 'node:process'
 </script>
 
 <script lang="ts">
+interface UserProfile {
+  country: string;
+  display_name: string;
+  email: string;
+  explicit_content: {
+    filter_enabled: boolean,
+    filter_locked: boolean
+  },
+  external_urls: { spotify: string; };
+  followers: { href: string; total: number; };
+  href: string;
+  id: string;
+  images: Image[];
+  product: string;
+  type: string;
+  uri: string;
+}
+
+interface Image {
+  url: string;
+  height: number;
+  width: number;
+}
+
+const clientId = import.meta.env.VITE_SPTFYID;
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+if (!code) {
+  console.log(clientId, import.meta.env)
+  //redirectToAuthCodeFlow(clientId);
+} else {
+  const accessToken = await getAccessToken(clientId, code);
+  const profile = await fetchProfile(accessToken);
+  populateUI(profile);
+}
+
+function populateUI(profile: UserProfile) {
+  console.log('populate', profile)
+}
+
+export async function getAccessToken(clientId: string, code: string): Promise<string> {
+  const verifier = localStorage.getItem("verifier");
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", "http://127.0.0.1:5173/callback");
+  params.append("code_verifier", verifier!);
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params
+  });
+
+  const { access_token } = await result.json();
+  return access_token;
+}
+
+async function fetchProfile(token: string): Promise<UserProfile> {
+  const result = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET", headers: { Authorization: `Bearer ${token}` }
+  });
+
+  return await result.json();
+}
+
+function generateCodeVerifier(length: number) {
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+async function generateCodeChallenge(codeVerifier: string) {
+  const data = new TextEncoder().encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest('SHA-256', data);
+  return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+}
+
+export async function redirectToAuthCodeFlow(clientId: string) {
+  const verifier = generateCodeVerifier(128);
+  const challenge = await generateCodeChallenge(verifier);
+
+  localStorage.setItem("verifier", verifier);
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("response_type", "code");
+  params.append("redirect_uri", "http://127.0.0.1:5173/callback");
+  params.append("scope", "user-read-private user-read-email");
+  params.append("code_challenge_method", "S256");
+  params.append("code_challenge", challenge);
+
+  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
 export default {
   data: () => ({ drawer: true, rail: true }),
 }
@@ -45,6 +151,17 @@ export default {
     </v-app-bar>
 
     <v-main>
+      <section id="profile">
+        <h2>Logged in as <span id="displayName"></span></h2>
+        <span id="avatar"></span>
+        <ul>
+          <li>User ID: <span id="id"></span></li>
+          <li>Email: <span id="email"></span></li>
+          <li>Spotify URI: <a id="uri" href="#"></a></li>
+          <li>Link: <a id="url" href="#"></a></li>
+          <li>Profile Image: <span id="imgUrl"></span></li>
+        </ul>
+      </section>
       <RouterView />
     </v-main>
   </v-app>
