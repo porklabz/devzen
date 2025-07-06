eae149772ccfa3d0693da5cdc70d8bf960fc75fb
Guilherme Fontenele <guilherme@fontenele.net>
2025-07-06 20:37:29 -0300

    fix: login spotify production url

--- src/App.vue
+++ src/App.vue
@@ -140,7 +140,7 @@ export default {
       const params = new URLSearchParams();
       params.append("client_id", this.sptfId);
       params.append("response_type", "code");
-      params.append("redirect_uri", "http://127.0.0.1:5173/callback");
+      params.append("redirect_uri", "https://dvzen.netlify.app/callback");
       params.append("scope", "user-read-private user-read-email");
       params.append("code_challenge_method", "S256");
       params.append("code_challenge", challenge);
